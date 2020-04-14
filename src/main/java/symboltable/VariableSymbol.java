7b28c10d0c0a2309a17d431a631fab56c7b14ad3
sinofp <sinofp@tuta.io>
2020-04-14 22:10:24 +0800

    feat(symtab): 改进Symbol、Scope输出
    之前输出变量是<a:Number>，但现在支持List<xxx>了，还用尖括号包裹变量会乱。
    
    打印作用域时会用逗号隔开Symbol，干脆扔掉包裹Symbol的尖括号好了。

--- src/main/java/symboltable/VariableSymbol.java
+++ src/main/java/symboltable/VariableSymbol.java
@@ -4,4 +4,9 @@ public class VariableSymbol extends Symbol {
     public VariableSymbol(String name, Type type) {
         super(name, type);
     }
+
+    @Override
+    public String toString() {
+        return "var " + super.toString();
+    }
 }
