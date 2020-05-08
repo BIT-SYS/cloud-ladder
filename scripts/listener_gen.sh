7bb1afbfc0756668ec90fabb61217342e7100592
sinofp <sinofp@tuta.io>
2020-05-08 21:58:37 +0800

    fix(chore): 把生成Listern脚本里的AST包改成ast

--- scripts/listener_gen.sh
+++ scripts/listener_gen.sh
@@ -1,7 +1,7 @@
 # help to generate ASTParserListener
-#SRC_NAME=../src/main/java/AST.ASTParser.java
-SRC_NAME=../src/main/java/AST
-DEST_DIR=../src/main/java/AST
+#SRC_NAME=../src/main/java/ast.ASTParser.java
+SRC_NAME=../src/main/java/ast
+DEST_DIR=../src/main/java/ast
 ARG=""
 BEFORE_FUNC='\tpublic void '
 CLASS_NAME="ASTBaseListener"
@@ -12,7 +12,7 @@ REMOVE_MATCH="AST\|Temp"
   #| grep -v "$REMOVE_MATCH" \
   #| awk -v ARG="$ARG" -v BEFORE_FUNC="$BEFORE_FUNC" \
     #-v CLASS_NAME="$CLASS_NAME" \
-  #-f ./listener_gen.awk > $DEST_DIR/AST.ASTBaseListener.java
+  #-f ./listener_gen.awk > $DEST_DIR/ast.ASTBaseListener.java
 
 ls $SRC_NAME -1\
   | tr '.' ' ' \
